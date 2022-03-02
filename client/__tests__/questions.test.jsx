21379a27543aa40bbb8ffa0105d66965222a3bac
Christoffer <links685@gmail.com>
2022-03-02 14:41:53 +0100

    first test passing. Also added husky

---
+++ client/__tests__/questions.test.jsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+
+import {QuizApplication} from "../quiz";
+
+
+describe("quiz application", () => {
+
+
+  it("shows front page", () => {
+    const element = document.createElement("div");
+    ReactDOM.render(<QuizApplication/>, element)
+
+    expect(element.querySelector("h1").innerHTML)
+      .toEqual("QuizApplication")
+    expect(element.innerHTML).toMatchSnapshot()
+  })
+})
